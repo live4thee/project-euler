42d010cbbddf10f9be9ef2ded95906910856491d
David Lee <live4thee@gmail.com>
2012-06-21 09:38:30 +0800

    Added a simple README file.

--- p14.fs
+++ p14.fs
@@ -1,5 +1,6 @@
 let isOdd (num: bigint) = num % 2I = 1I
 let nextNum (n: bigint) = if isOdd n then 3I * n + 1I else n / 2I
+
 let numCache = new System.Collections.Generic.Dictionary<bigint, int>()
 
 let rec get_series_counter (cur_num: bigint) =
@@ -19,12 +20,14 @@ let solve_p14 (max_num: bigint) =
       let next_num = cur_num + 1I in
       let acc_num' = if cnt > acc_cnt then cur_num else acc_num in
       let acc_cnt' = if cnt > acc_cnt then cnt else acc_cnt in
-        printf "%O -> %d\n" cur_num cnt;
         solver next_num acc_num' acc_cnt'
   in
     numCache.Add(1I, 1);
     numCache.Add(2I, 2);
     numCache.Add(3I, 8);
+    numCache.Add(5I, 6);
+    numCache.Add(7I, 17);
     solver 3I 2I 2
 
-printfn "%O" (solve_p14 1000I)
+(* Works in F# interpretor.  Compiled EXE will stack overflow. *)
+ printfn "%O" (solve_p14 1000000I)
