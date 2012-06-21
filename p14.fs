9bcacfe0344929152f27b2cd16b74885e1aae3b7
David Lee <live4thee@gmail.com>
2012-06-21 14:48:59 +0800

    Replace try/except with if/else to avoid StackOverflowExcetpion

--- p14.fs
+++ p14.fs
@@ -4,9 +4,8 @@ let nextNum (n: bigint) = if isOdd n then 3I * n + 1I else n / 2I
 let numCache = new System.Collections.Generic.Dictionary<bigint, int>()
 
 let rec get_series_counter (cur_num: bigint) =
-  try
-    numCache.[cur_num]
-  with exn ->
+  if numCache.ContainsKey(cur_num) then numCache.[cur_num]
+  else
     let next_num = nextNum cur_num in
     let count = get_series_counter next_num in
       numCache.Add(cur_num, count + 1);
@@ -29,5 +28,4 @@ let solve_p14 (max_num: bigint) =
     numCache.Add(7I, 17);
     solver 3I 2I 2
 
-(* Works in F# interpretor.  Compiled EXE will stack overflow. *)
- printfn "%O" (solve_p14 1000000I)
+printfn "%O" (solve_p14 1000000I)
